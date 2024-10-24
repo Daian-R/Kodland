93fd77efc9bb0be7f10736d10c1d08bf78a9860c
Daian-R <brop73481@gmail.com>
2024-10-24 16:40:43 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,42 @@
+import telebot
+from test import password_generator, token
+    # Замени 'TOKEN' на токен твоего бота
+    # Этот токен ты получаешь от BotFather, чтобы бот мог работать
+bot = telebot.TeleBot(token)
+    
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши что-нибудь!")
+    
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Привет! Как дела?")
+    
+@bot.message_handler(commands=['bye'])
+def send_bye(message):
+    bot.reply_to(message, "Пока! Удачи!")
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "/start, /hello, /bye")
+    
+@bot.message_handler(commands=['password'])
+def send_password(message):
+    parts = message.text.split()
+    if len(parts) > 1:
+        count_pas = int(parts[1])
+    else:
+        count_pas = 10
+    print(count_pas)
+    bot.reply_to(message, password_generator(count_pas))
+
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+    
+bot.polling() 
